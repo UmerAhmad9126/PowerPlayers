84d03731cabd59f162c7d92850c833498ab22d89
UmerAhmad9126 <ahmadumer9126@gmail.com>
2023-04-08 21:12:51 +0530

    tailwind css added

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div >
+            <div className='border border-sky-500 mt-60'>
+                <h1 className='decoration-solid text-xs'>SOS GAME</h1>
+            </div>
+        </div>
     )
 }
 
