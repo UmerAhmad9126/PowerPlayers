b97398531eebdae18f3a939f804bc29b2082d221
Madhur Nadamwar <112754729+madhurn29@users.noreply.github.com>
2023-04-09 01:07:04 +0530

    backend project

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,15 @@
 const express = require("express");
+const { connection } = require("./Configs/db");
+
+const app = express();
+require("dotenv").config();
+
+app.listen(process.env.PORT,async()=>{
+    try {
+        await connection
+        console.log("Connected to Database")        
+    } catch (error) {
+        console.log(error.message)
+    }
+    console.log(`Port runnning on ${process.env.PORT}`)
+})
--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
-    return (
-        <div >
-            <div className='border border-sky-500 mt-60'>
-                <h1 className='decoration-solid text-xs'>SOS GAME</h1>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <div className="border border-sky-500 mt-60">
+        <h1 className="decoration-solid text-xs">SOS GAME</h1>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
