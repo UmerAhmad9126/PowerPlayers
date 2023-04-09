f9dd312dafb4fde9b2bc8641abc5a6036cf6d02f
UmerAhmad9126 <ahmadumer9126@gmail.com>
2023-04-09 23:32:37 +0530

    whole wrok added

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(cors({ origin: "*" }));
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://frontend-one-eta-39.vercel.app",
     methods: ["GET", "POST"],
   },
 });
